dc1e5078e04d2f3dafef697dcd0a9fdfc98dd445
Precious Aleaji <legenduz15@gmail.com>
2023-12-08 06:57:28 +0100

    Add console.py
    Co-authored-by: El Houssain Souhail <sou.ductive@gmail.com>

---
+++ console.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+AirBnB console. Extends pythons's cmd module
+"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """
+    The HBNBCommand class
+    Forms the basis for the console repl
+
+    Attributes:
+        prompt (str): Custom prompt '(hbnb)'
+    """
+    prompt = '(hbnb) '
+
+    def do_EOF(self, line):
+        """
+        Handles 'EOF'
+        """
+        print()
+        return True
+
+    def do_quit(self, line):
+        """
+        Quit command to exit the program
+        """
+        return True
+
+    def emptyline(self):
+        """
+        Catches empty lines and does nothing
+        """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
