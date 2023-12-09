9d5bc6570edfe6921f037989e4d0c030714244a6
El Houssain Souhail <sou.ductive@gmail.com>
2023-12-09 19:54:28 +0100

    added new test + styling changes

---
+++ tests/test_console.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Unittest module for the Console
+"""
+import unittest
+
+
+class TestInstantiation(unittest.TestCase):
+    """
+    Instantiation test
+    -> To be added
+    """
+    def test_init(self):
+        """tests"""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
