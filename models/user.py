36de8bd204739c754d79820200dc73b9093192a9
Precious Aleaji <legenduz15@gmail.com>
2023-12-11 06:59:52 +0100

    Update user

--- models/user.py
+++ models/user.py
@@ -19,7 +19,3 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """User Instantiation"""
-        super().__init__(*args, **kwargs)
